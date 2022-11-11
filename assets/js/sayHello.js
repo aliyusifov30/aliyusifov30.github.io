17281b3a879ae9c5b35c06cf4cd126791bd8829e
Ali Yusifov <aliyusifovoffical@mail.ru>
2022-11-11 09:49:27 +0400

    added to

--- assets/js/sayHello.js
+++ assets/js/sayHello.js
@@ -2,21 +2,11 @@
 
 let load = document.querySelector(".load")
 
-
 window.addEventListener("load",function(){
-    load.style.zIndex = "1";
-    load.style.background = "black";
-
+    load.style.display = "none";
 })
 
-// var response = video.addEventListener("canplaythrough", function() {
-//   // Ready to play whole video?
-//   console.log("assd")
-// },false);
 
+window.addEventListener("resize",function(){
 
-
-// video.addEventListener('canplaythrough', (event) => {
-//   console.log('I think I can play through the entire ' +
-//       'video without ever having to stop to buffer.');
-// });
+})
