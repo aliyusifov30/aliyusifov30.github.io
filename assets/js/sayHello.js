4fde519fc81dd29c8391d110a4686e9c06afdb09
Ali Yusifov <aliyusifovoffical@mail.ru>
2022-11-12 16:10:18 +0400

    123

--- assets/js/sayHello.js
+++ assets/js/sayHello.js
@@ -1,12 +1,14 @@
 
 
 let load = document.querySelector(".load")
+let body = document.querySelector(".sayHello")
+
+body.style.overflow = "hidden";
+load.style.transition = "100";
 
 window.addEventListener("load",function(){
-    load.style.display = "none";
-})
-
-
-window.addEventListener("resize",function(){
-
+    load.style.opacity = "0";
+    load.style.transition = "2s";
+    console.log(body)
+    body.style.overflow = "auto !important";
 })
